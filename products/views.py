726a160323aa965a31ba0cfd5a2e100debf689ae
cobyfoster <cobyfoster99@gmail.com>
2021-01-29 19:55:19 +0000

    added filtering to url

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
-from .models import Product
+from .models import Product, Category
 from django.db.models import Q
 from django.contrib import messages
 from django.http import HttpResponse
@@ -12,18 +12,27 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
+
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "Please enter some text to search")
                 return redirect(reverse('products'))
 
-            queries = Q(model_name__icontains=query) | Q(description__icontains=query) | Q(device_type__icontains=query) | Q(cpu__icontains=query) | Q(cpu__icontains=query) | Q(gpu__icontains=query)
+            queries = Q(model_name__icontains=query) | Q(description__icontains=query) | Q(device_type__icontains=query) | Q(cpu__icontains=query) | Q(ram__icontains=query) | Q(gpu__icontains=query)
             products = products.filter(queries)
 
     context = {
-        'products': products
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
     return render(request, 'products/products.html', context)
 
