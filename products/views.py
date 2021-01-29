f227d57ee8cc42deefd3c3d4f7d579b9251756ec
cobyfoster <cobyfoster99@gmail.com>
2021-01-29 20:25:09 +0000

    sorting added

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from .models import Product, Category
 from django.db.models import Q
 from django.contrib import messages
 from django.http import HttpResponse
+from django.db.models.functions import Lower
 
 # Create your views here.
 
@@ -13,8 +14,22 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
-
+    sort = None
+    direction = None
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'model_name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('model_name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -26,14 +41,19 @@ def all_products(request):
                 messages.error(request, "Please enter some text to search")
                 return redirect(reverse('products'))
 
-            queries = Q(model_name__icontains=query) | Q(description__icontains=query) | Q(device_type__icontains=query) | Q(cpu__icontains=query) | Q(ram__icontains=query) | Q(gpu__icontains=query)
+            queries = Q(model_name__icontains=query) | Q(description__icontains=query) | Q(
+                device_type__icontains=query) | Q(cpu__icontains=query) | Q(ram__icontains=query) | Q(gpu__icontains=query)
             products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
+
     return render(request, 'products/products.html', context)
 
 
