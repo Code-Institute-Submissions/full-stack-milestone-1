4f669d81c06644d08577889048f64035125b673d
cobyfoster <cobyfoster99@gmail.com>
2021-01-30 23:04:24 +0000

    Toasts included

--- cart/views.py
+++ cart/views.py
@@ -10,7 +10,7 @@ def view_cart(request):
 
 
 def add_to_cart(request, item_id):
-
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -19,6 +19,7 @@ def add_to_cart(request, item_id):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
+        messages.success(request, f'{product.model_name} has been succesfully added to your cart')
 
     request.session['cart'] = cart
     print(request.session['cart'])
