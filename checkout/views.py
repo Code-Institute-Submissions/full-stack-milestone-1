8df0dc4e48d022d610ce0f326f683ddf2032e7a5
cobyfoster <cobyfoster99@gmail.com>
2021-01-31 11:32:12 +0000

    Stripe checkout setup using mini project

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from .forms import OrderForm
+from django.conf import settings
 
 # Create your views here.
 
@@ -15,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+        'stripe_secret_key': settings.STRIPE_SECRET_KEY,
     }
 
     return render(request, template, context)
