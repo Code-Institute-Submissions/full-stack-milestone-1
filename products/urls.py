80bcde3c3aef0451641fef18526b117adc73d71d
cobyfoster <cobyfoster99@gmail.com>
2021-01-31 20:17:52 +0000

    Edit functionality working

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('add/', views.add_product, name='add_product'),
-    path('<product_id>/', views.product_single, name='product_single'),
-
+    path('<int:product_id>/', views.product_single, name='product_single'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
