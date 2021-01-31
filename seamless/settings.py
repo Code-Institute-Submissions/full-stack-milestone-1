14d136696d22bf6762524d0a3231fe21a87e797a
cobyfoster <cobyfoster99@gmail.com>
2021-01-31 22:01:14 +0000

    Enabled caching

--- seamless/settings.py
+++ seamless/settings.py
@@ -169,6 +169,11 @@ FREE_DELIVERY_THRESHOLD = 800
 STANDARD_DELIVERY_COST = 5
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     AWS_STORAGE_BUCKET_NAME = 'full-stack-milestone'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
